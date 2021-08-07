e269150e2a2f5e27c7371c61cca41cf6b238e958
Traffic <trafficconegod@gmail.com>
2021-08-06 20:17:10 -0400

    balls

--- Source/Actors/BouncingBallGenerator.cpp
+++ Source/Actors/BouncingBallGenerator.cpp
@@ -5,5 +5,7 @@ void BouncingBallGenerator::ButtonPressed(uint buttonType) {
     if (buttonType & WPAD_BUTTON_A) {
         DataStream stream(MainStage.buf, MainStage.size);
         CreateChildFrom(stream);
+    } else if (buttonType & WPAD_BUTTON_B) {
+        Destroy();
     }
 }
