28955a2fbc177bb7dad27d27f206959d25b9c06a
Traffic <trafficconegod@gmail.com>
2021-08-13 13:34:37 -0400

    fix fucking stupid ass bug

--- buildImage.sh
+++ buildImage.sh
@@ -1,4 +1,4 @@
-test ! -f Image.iso && mksdcard 10M Image.iso
+test ! -f Image.iso && mksdcard 50M Image.iso
 sudo mount -o defaults,umask=000 Image.iso Media/SDCard
 cp -r Data Media/SDCard
 sudo umount Media/SDCard
